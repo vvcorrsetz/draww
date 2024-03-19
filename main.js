127584a42c51d016627082bf02558a050df7e9c4
vvcorrsetz <111374808+vvcorrsetz@users.noreply.github.com>
2024-03-19 19:00:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+array=["pen" , "clock", "spoon" , "cake" ,"cat","book","axe","milkske","iphone","dog"]
+randomNumber= Math.floor((Math.random()*array)+1)
+element=array[randomNumber]
+document.getElementById("sketch").innerHTML="sketch  to be drawn:"+element
+timecounter=0
+timercheck=""
+drawsketch=""
+answer=""
+score=0
+
